196ef51d031b34b055e506dd71f6dcdcd636f040
Sushil <kumarsushil0370@gmail.com>
2021-11-29 20:31:46 +0530

    Recovered the files structure to working condition. Particle js needs to be changed.
    Make sure Nik to do it... Its your responsibility now.

--- InfoSnackDjango/templates/SnackIdea/frontend-infosnack/src/components/2SignIn.jsx
+++ InfoSnackDjango/templates/SnackIdea/frontend-infosnack/src/components/2SignIn.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
+
 const SignIn = () => {
     return (
         <div>
-            Hello I am SignIn pAge.
+            Hello I am SignUp Page.
+            <form className="signin">
+                <input type="submit" value="User Name" />
+                <input type="submit" value="Password" />
+                <button type="submit">Login</button>
+            </form>
         </div>
     )
 }
